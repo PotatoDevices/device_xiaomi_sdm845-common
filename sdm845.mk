fd9356d48f16035cd74a821db18133af9e8e3cf5
Bruno Martins <bgcngm@gmail.com>
2019-02-16 22:22:04 +0000

    sdm845-common: Populate initial list of common vendor blobs
     * Common vendor repo will be created from perseus, which will
       eventually allow runtime linker namespace isolation to be enabled.
    
     * This goes along with the rest of basic shared libraries that can
       be built from source right away.
    
    Change-Id: I944f210685d980c0603fd2c9bcd9defbfb0209c2

--- sdm845.mk
+++ sdm845.mk
@@ -68,6 +68,8 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@4.0-impl \
     android.hardware.soundtrigger@2.1-impl \
     audio.a2dp.default \
+    audio.r_submix.default \
+    audio.usb.default \
     tinymix
 
 PRODUCT_COPY_FILES += \
@@ -76,7 +78,8 @@ PRODUCT_COPY_FILES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service
+    android.hardware.camera.provider@2.4-service \
+    vendor.qti.hardware.camera.device@1.0.vendor
 
 # Common init scripts
 PRODUCT_PACKAGES += \
@@ -97,8 +100,11 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
+    libdrm.vendor \
+    libgui_vendor \
+    libtinyxml \
     libvulkan \
-    vendor.display.config@1.0
+    vendor.display.config@1.2.vendor
 
 # DRM
 PRODUCT_PACKAGES += \
@@ -137,6 +143,11 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     ims-ext-common
 
+# IPA
+PRODUCT_PACKAGES += \
+    libnetfilter_conntrack \
+    libnfnetlink
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_sdm845
@@ -159,6 +170,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml \
     $(LOCAL_PATH)/permissions/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml
+
+# Radio
+PRODUCT_PACKAGES += \
+    libjson \
+    librmnetctl
 
 # RCS
 PRODUCT_PACKAGES += \
@@ -183,7 +199,8 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-service \
+    libsensorndkbridge
 
 # Telephony
 PRODUCT_PACKAGES += \
