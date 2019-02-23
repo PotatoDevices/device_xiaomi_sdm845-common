4d4d08830fe389d1dc9a39740ad44391d733b1d5
Bruno Martins <bgcngm@gmail.com>
2019-02-23 23:46:48 +0000

    sdm845-common: Switch to vendor variants of the custom HALs
    Change-Id: I599ef8b3d143641821b6e42ec41050aedd3f2b37

--- sdm845.mk
+++ sdm845.mk
@@ -125,7 +125,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/fingerprint/android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sdm845.rc:system/etc/init/android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sdm845.rc
+    $(LOCAL_PATH)/fingerprint/android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sdm845.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sdm845.rc
 
 # Health
 PRODUCT_PACKAGES += \
@@ -175,7 +175,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.0-impl \
     android.hardware.power@1.0-service \
-    power.qcom:64
+    power.sdm845
 
 # QTI
 PRODUCT_COPY_FILES += \
