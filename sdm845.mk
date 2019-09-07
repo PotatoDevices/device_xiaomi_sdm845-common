9f927b14a7745b686168a35d8a96d5b0dc541527
Kshitij Gupta <kshitijgm@gmail.com>
2019-09-07 21:55:38 +0530

    sdm845-common: Remove extraneous inherits
    - These files are inherited one way or another, already
    
    Change-Id: Ie06423bfbcf08b1847cfc03a55a4894e3bf91ad2
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -6,10 +6,6 @@
 
 COMMON_PATH := device/xiaomi/sdm845-common
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/xiaomi/sdm845-common/sdm845-common-vendor.mk)
 
