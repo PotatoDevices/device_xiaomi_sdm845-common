8687eb3917d3f6e179997f66e90874898adc0fbd
Kshitij Gupta <kshitijgm@gmail.com>
2019-09-10 18:06:48 +0530

    sdm845-common: Build broken dup rules and phony targets
    Change-Id: Ie7217dd182ca319a93277533cb385b270952f247
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -27,6 +27,10 @@ TARGET_USES_64_BIT_BINDER := true
 TARGET_BOOTLOADER_BOARD_NAME := sdm845
 TARGET_NO_BOOTLOADER := true
 
+# Broken rules
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_PHONY_TARGETS := true
+
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xA84000 androidboot.hardware=qcom androidboot.console=ttyMSM0 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 swiotlb=2048 androidboot.configfs=true loop.max_part=7 androidboot.usbcontroller=a600000.dwc3
