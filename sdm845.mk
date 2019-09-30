410451a9a15d9604929edc6afa4abf0a37bd134d
Bruno Martins <bgcngm@gmail.com>
2019-09-30 13:32:04 +0100

    sdm845-common: Drop mediaextractor seccomp policy
    These syscalls are now allowed by default.
    
    Change-Id: I405f609d030331e19031612cbd912a54a3c72339

--- sdm845.mk
+++ sdm845.mk
@@ -275,8 +275,7 @@ PRODUCT_SOONG_NAMESPACES += $(COMMON_PATH)
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
 # Sensors
 PRODUCT_PACKAGES += \
