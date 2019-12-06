0c53a07e67932fdcda57be2ab49b7a6adfabc942
Lokesh Gidra <lokeshgidra@google.com>
2019-12-06 14:00:16 -0800

    sdm845-common: Use 6GB dalvik-heap config
    We are currently using 2GB dalvik-heap config.
    
    Bug: 145823510
    Test: MPTS
    Change-Id: I086d4a5c09634c3c7bd51ec5d08dcaa1551511b4
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -15,6 +15,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
