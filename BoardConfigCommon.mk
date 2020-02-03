e3abf33fd90df37f7edc8a3046472718959d332a
Bruno Martins <bgcngm@gmail.com>
2020-02-03 22:03:45 +0000

    sdm845-common: Bump security patch level to match MIUI 11
    Change-Id: I4a74b696806b99ff1dfd7477b0faeeaf7cc73df5
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -132,6 +132,9 @@ OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 ENABLE_VENDOR_RIL_SERVICE := true
 TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
+# Security patch level
+VENDOR_SECURITY_PATCH := 2020-01-01
+
 # Sepolicy
 include device/qcom/sepolicy/sepolicy.mk
 
