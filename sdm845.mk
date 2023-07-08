223e09cb9907ed867c67a48fd897f9cc51dd2424
SahilSonar <sss.sonar2003@gmail.com>
2023-07-08 11:06:22 +0200

    sdm845-common: Add potato soong namespace

--- sdm845.mk
+++ sdm845.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+PRODUCT_BOARD_PLATFORM := sdm845
+PRODUCT_USES_QCOM_HARDWARE := true
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
